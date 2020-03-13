de6bcab2a71f4808f3cd4dd482a20d577fd46e94
Aleksey Belan <alexey.belan@spryker.com>
2020-03-13 14:06:45 +0200

    BO-171: PR fixes

--- src/Spryker/Zed/ZedUi/Presentation/Components/app/header/header.component.ts
+++ src/Spryker/Zed/ZedUi/Presentation/Components/app/header/header.component.ts
@@ -1,11 +1,11 @@
-import {ChangeDetectionStrategy, Component, ViewEncapsulation} from '@angular/core';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
-  selector: 'mp-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.less'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  encapsulation: ViewEncapsulation.None,
+    selector: 'mp-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.less'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    encapsulation: ViewEncapsulation.None,
 })
 export class HeaderComponent {
 }
